75bb6c652f89da2844a77fab80198ad3d58a049d
Carlos-Soto <carlossebastian.sotocastaneda.stu@ispascalcomandini.it>
2022-02-11 09:06:07 +0100

    2v

--- Scuola/Scuola/scuola.cs
+++ Scuola/Scuola/scuola.cs
@@ -7,12 +7,16 @@ namespace Scuola
     class scuola
     {
 
-        string nome;
-        string cognome;
-        string ore;
+        string Nome { get; set; }
+
+        string Cognome { get; set; }
+        int Ore { get; set; }
+
         private scuola(string nome,string cognome,int ore)
         {
-
+            Nome = nome;
+            Cognome = cognome;
+            Ore = ore;
         }
 
     }
