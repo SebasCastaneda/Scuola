3fc010cc84416ab431b7af2cebf7793d105d7877
Carlos-Soto <carlossebastian.sotocastaneda.stu@ispascalcomandini.it>
2022-02-11 08:51:19 +0100

    1ver

---
+++ Scuola/Scuola/Alunno.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scuola
+{
+    class Alunno
+    {
+
+
+        public string ANNO (int eta)
+            {
+              string classe = "";
+                if(eta>=17)
+                {
+                classe="trienio";
+
+                 }
+             else
+            {
+
+                classe="biennio";
+             }
+        }
+
+
+    }
+       
+
+}
+ 
+
