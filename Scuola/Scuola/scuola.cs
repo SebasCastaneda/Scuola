e6a5243fc4834b7a50c2471ceeb07650d725ddf7
Carlos-Soto <carlossebastian.sotocastaneda.stu@ispascalcomandini.it>
2022-02-11 09:02:00 +0100

    1v

---
+++ Scuola/Scuola/scuola.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scuola
+{
+    class scuola
+    {
+
+        string nome;
+        string cognome;
+        string ore;
+        private scuola(string nome,string cognome,int ore)
+        {
+
+        }
+
+    }
+}
